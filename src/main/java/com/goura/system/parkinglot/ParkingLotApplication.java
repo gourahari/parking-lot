7453daaf71da610477d410135afee4a6048a9c07
gourahari <gourahari.das@gmail.com>
2023-01-07 03:21:12 -0500

    sleuth reference removed.

--- src/main/java/com/goura/system/parkinglot/ParkingLotApplication.java
+++ src/main/java/com/goura/system/parkinglot/ParkingLotApplication.java
@@ -2,7 +2,6 @@ package com.goura.system.parkinglot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.autoconfig.instrument.web.ConditionalOnSleuthHttp;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
